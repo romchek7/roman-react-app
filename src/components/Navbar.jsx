dc34c85a6e878937e0f5fbf780f1b024415353ec
Roman <sonecccbbbaaa@gmail.com>
2022-06-23 17:26:37 +0300

    Create react app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <a href="#">
+                <div>Home</div>
+            </a>
+            <a href="#">
+                <div>Profile</div>
+            </a>
+            <a href="#">
+                <div>News</div>
+            </a>
+            <a href="#">
+                <div>About us</div>
+            </a>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>Logo</div>
+            <div>Info</div>
+            <div>Posts</div>
+            <div>Post1</div>
+        </div>
+    );
+}
+
+export default Profile;
