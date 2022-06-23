9e6bf0a8af27fd03a7677446bc1c1a2387384ebc
Roman <sonecccbbbaaa@gmail.com>
2022-06-23 17:40:58 +0300

    Delete header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
 import Footer from './components/Footer';
@@ -8,7 +7,6 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <Header />
       <Navbar />
       <Profile />
       <Footer />
