e526ada7b8dbe653d3dd46afa8c7b346d06b03dc
romchek7 <sonecccbbbaaa@gmail.com>
2022-07-28 14:35:54 +0300

    54. componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,9 +4,7 @@ import userIcon from '../img/account.png'
 import * as axios from 'axios'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-
+    componentDidMount() {
         if (this.props.usersPage.users.length === 0) {
             axios
                 .get('https://social-network.samuraijs.com/api/1.0/users')
