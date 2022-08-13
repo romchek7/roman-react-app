a479a47e3ea014aca2f8f9f1f159473e416eb673
romchek7 <sonecccbbbaaa@gmail.com>
2022-08-13 14:39:45 +0300

    75. reduxForm for login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import {Field, reduxForm} from "redux-form";
+import styles from './Login.module.css'
+
+let LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div className={styles.formDiv}>
+                <div className={styles.formItem}>
+                    <Field placeholder={'Login'} name={'login'} component={'input'}/>
+                </div>
+                <div className={styles.formItem}>
+                    <Field placeholder={'Password'} name={'password'} component={'input'}/>
+                </div>
+                <div className={styles.formItem}>
+                    <Field name={'rememberMe'} component={'input'} type={'checkbox'}/> remember me
+                </div>
+                <div className={styles.formItem}>
+                    <button>Login</button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({
+    form: 'login'
+})(LoginForm)
 
 let Login = () => {
-    return (
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+
+    return <div className={styles.main}>
         <h1>LOGIN</h1>
-    )
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
 }
 
 export default Login
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,12 +4,14 @@ import dialogsPageReducer from "./dialogsPageReducer";
 import usersPageReducer from "./usersPageReducer";
 import authReducer from "./auth-reducer";
 import thunk from "redux-thunk";
+import {reducer as formReducer} from "redux-form";
 
 let reducers = combineReducers({
     profilePage: profilePageReducer,
     dialogsPage: dialogsPageReducer,
     usersPage: usersPageReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 let store = createStore(reducers, applyMiddleware(thunk))
